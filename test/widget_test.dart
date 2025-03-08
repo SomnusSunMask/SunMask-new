87e85a3158e3b7171a86833d8ab32f3e8d15ca1b
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-09 00:26:09 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_ble2/main.dart'; // Stelle sicher, dass `main.dart` existiert
+import 'package:flutter_ble2/main.dart'; // Sicherstellen, dass der Pfad stimmt
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp()); // Entferne `const`, falls nötig
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Counter: 0'), findsOneWidget);
+    expect(find.text('Counter: 1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Increment'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Counter: 0'), findsNothing);
+    expect(find.text('Counter: 1'), findsOneWidget);
   });
 }
