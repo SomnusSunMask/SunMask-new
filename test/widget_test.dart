5e9c45a458bf1658fb0d8490e667869ad6eb1d8c
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-09 10:33:02 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ble2/main.dart';
 
 void main() {
-  testWidgets('BLE App UI Test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.text('Start Scan'), findsOneWidget);
-    expect(find.text('Stop Scan'), findsOneWidget);
-    
-    await tester.tap(find.text('Start Scan'));
-    await tester.pump();
-    
-    expect(find.text('Stop Scan'), findsOneWidget);
+  testWidgets('App startet ohne Fehler', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    expect(find.text('BLE Scanner'), findsOneWidget);
   });
 }
