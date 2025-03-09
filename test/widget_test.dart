867620697296d6be28471dc7efa15f7c93fed0c0
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-09 10:17:11 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ble2/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('BLE App UI Test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.text('Start Scan'), findsOneWidget);
