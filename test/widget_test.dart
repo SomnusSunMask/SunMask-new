7836335f92dcdd41fce06e0d99a2580c5a56fbbf
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-09 10:12:20 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,17 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ble2/main.dart';
 
 void main() {
-  testWidgets('BLE HomePage Test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
 
-    // Prüfen, ob der Button existiert
     expect(find.text('Start Scan'), findsOneWidget);
+    expect(find.text('Stop Scan'), findsOneWidget);
     
-    // Button klicken und Scan starten
     await tester.tap(find.text('Start Scan'));
     await tester.pump();
-
-    // Nach dem Scan sollte "Scanning..." angezeigt werden
-    expect(find.text('Scanning...'), findsOneWidget);
+    
+    expect(find.text('Stop Scan'), findsOneWidget);
   });
 }
