5304d710278055a0a327aff965002ba642f41c55
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-09 10:47:47 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ble2/main.dart';
 
 void main() {
   testWidgets('App startet ohne Fehler', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.text('BLE Scanner'), findsOneWidget);
+    // App starten
+    await tester.pumpWidget(const MyApp());
+
+    // Prüfen, ob das Haupt-Widget existiert
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.byType(BLEHomePage), findsOneWidget);
   });
 }
