92088d610f75ce09ca7e70396ab99203ca4b0c52
SunMask1 <cavitas.pelvis@protonmail.com>
2025-03-12 15:44:11 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_ble2/main.dart';
+import 'package:SunMask/main.dart';
 
 void main() {
   testWidgets('App startet ohne Fehler', (WidgetTester tester) async {
