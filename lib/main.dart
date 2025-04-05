e05fc0b77f9761918e4fcac724d90cf94ba8391b
SunMask1 <cavitas.pelvis@protonmail.com>
2025-04-05 23:13:29 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1023,6 +1023,7 @@ class _DeviceOverviewPageState extends State<DeviceOverviewPage> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: blaugrau,
                   foregroundColor: Colors.white,
+                  overlayColor: MaterialStateProperty.all(blaugrau), // <- Das ist die Verbesserung!
                 ),
                 onPressed: isConnecting ? null : connectToDeviceById,
                 child: Text(
