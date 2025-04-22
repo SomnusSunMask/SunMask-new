4de60f423250525b4fd32fe37e7790716ee9aef1
SomnusSunMask <constigo8@gmail.com>
2025-04-22 17:53:38 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,28 +11,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
-      theme: ThemeData.dark(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     final now = DateTime.now();
-    final formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+    final timeFormatted = DateFormat.Hm().format(now);
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("INTL Test")),
-      body: Center(
-        child: Text(
-          "Aktuelles Datum:\n$formattedDate",
-          style: const TextStyle(fontSize: 22),
-          textAlign: TextAlign.center,
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Plugin-Test')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Aktuelle Uhrzeit: $timeFormatted'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final state = await FlutterBluePlus.adapterState.first;
+                  debugPrint("Bluetooth-Status: $state");
+                },
+                child: const Text("Bluetooth prüfen"),
+              ),
+            ],
+          ),
         ),
       ),
     );
