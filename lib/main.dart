22cb26cbd59c8490470e68e0f76ab2cd8f2d4ace
SomnusSunMask <constigo8@gmail.com>
2025-04-22 17:36:32 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // main.dart
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,58 +12,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test SharedPreferences',
-      home: const TestPage(),
+      home: const MyHomePage(),
+      theme: ThemeData.dark(),
     );
   }
 }
 
-class TestPage extends StatefulWidget {
-  const TestPage({super.key});
-
-  @override
-  State<TestPage> createState() => _TestPageState();
-}
-
-class _TestPageState extends State<TestPage> {
-  String savedValue = "Noch nichts gespeichert";
-
-  Future<void> saveValue() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('key', 'Hallo Welt');
-    setState(() {
-      savedValue = 'Gespeichert: Hallo Welt';
-    });
-  }
-
-  Future<void> loadValue() async {
-    final prefs = await SharedPreferences.getInstance();
-    final value = prefs.getString('key');
-    setState(() {
-      savedValue = value ?? 'Kein Wert gefunden';
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: const Text('SharedPreferences Test')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text(savedValue, style: const TextStyle(fontSize: 20)),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: saveValue,
-              child: const Text('Wert speichern'),
-            ),
-            ElevatedButton(
-              onPressed: loadValue,
-              child: const Text('Wert laden'),
-            ),
-          ],
+      appBar: AppBar(title: const Text("INTL Test")),
+      body: Center(
+        child: Text(
+          "Aktuelles Datum:\n$formattedDate",
+          style: const TextStyle(fontSize: 22),
+          textAlign: TextAlign.center,
         ),
       ),
     );
